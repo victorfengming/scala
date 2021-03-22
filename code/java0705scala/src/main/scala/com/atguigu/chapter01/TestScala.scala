68b84775f37dda008a52fe741e8afa8440872184
于凤明 <yufengming@travelsky.com>
2021-03-22 16:16:42 +0800

    使用idea 开发的第一个scala程序

---
+++ code/java0705scala/src/main/scala/com/atguigu/chapter01/TestScala.scala
@@ -0,0 +1,22 @@
+package com.atguigu.chapter01
+
+/**
+ * ClassName: ${CLASS_NAME} <br/>
+ * Description: ${CLASS_NAME} <br/>
+ * Date: 2021-03-22 16:13 <br/>
+ * <br/>
+ *
+ * @author yufengming
+ * @email yufengming@travelsky.com
+ * @project scala
+ * @package com.atguigu.chapter01
+ * @version 产品版本信息 2021年03月22日16:13分 yufengming(yufengming@travelsky.com) 新建<br/>
+ *          <p>
+ *          修改记录
+ *
+ */
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("hello,scala,idea...")
+  }
+}
