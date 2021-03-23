3ebf74e6cb45b2e0bb7a9ce7dd5680a45de102ad
上官一明 <victorfengming@163.com>
2021-03-23 19:12:45 +0800

    看的有点儿块啊

---
+++ code/java0705scala/src/main/scala/com/atguigu/chapter04/ifelse/Demo01.scala
@@ -0,0 +1,22 @@
+package com.atguigu.chapter04.ifelse
+
+/**
+ *
+ * *
+ *
+ * @author victor
+ *         *
+ * @site https://victorfengming.gitee.io/
+ *       *
+ * @project scala
+ *          *
+ * @package com.atguigu.chapter04.ifelse
+ *          *
+ * @created 2021-03-23 18:59
+ */
+object Demo01 {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
